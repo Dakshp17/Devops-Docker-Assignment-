d3fb9579259fe372ee0c0f08067e51a142681ee9
Dakshp17 <dakshpatelh@gmail.com>
2025-12-07 12:06:21 +0530

    request method changed

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['get'])
 def submit():
     # Try getting JSON first, then form data
     data = request.get_json(silent=True)
